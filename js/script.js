56a56e41f3d6bdc32b2bf570094a54ad4636c5e5
Nicola <nicolusso@gmail.com>
2021-01-25 16:11:18 +0100

    function click finish

--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@ var app = new Vue({
   el: '#root',
   data: {
     message: 'Hello World'
+  },
+})
+  new Vue({
+  el: "#app",
+  data: {
+    change: false
+  },
+  methods: {
+    click() {
+      this.change = true;
+    }
   }
 })
-
-// methods: {
-//   click: function () {
-//
-//   }
-// }
